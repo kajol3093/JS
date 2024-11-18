580474ad26e28631bee40efa59caca333b42d793
kajol3093 <kshine3093@gmail.com>
2024-11-18 07:01:39 +0000

    container config done

---
+++ 01_variables.js
@@ -0,0 +1,13 @@
+const accountID= 144553
+let accountEmail="Kajol@gmail.com"
+var accountPassword="12345"
+accountCity= "Jaipur"
+
+console.log(accountID);
+
+accountEmail="kajol123@gmail.com"
+accountPassword="212121"
+accountCity="Kashmir"
+accountState= "MP"
+
+console.table([ accountID, accountEmail, accountPassword, accountCity, accountState])
