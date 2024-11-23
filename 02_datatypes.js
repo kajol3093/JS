ef44ac5548af1781d44adcf35458e1487b7cdeab
kajol3093 <kshine3093@gmail.com>
2024-11-23 07:36:56 +0000

    data types

---
+++ 02_datatypes.js
@@ -0,0 +1,19 @@
+let name="Kajol"
+let age= 30
+let isloggedin= false
+let state= null
+
+// bigint=
+//number=2 to the power 53
+//string- ""
+//booolean- true/false
+//null- standalone value
+// undefined=value not defined
+//symbol= unique
+
+console.log( typeof "Kajol")
+console.log( typeof age);
+console.log(typeof null);
+console.log(typeof undefined);
+
+
